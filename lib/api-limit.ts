5f9e19ad7eeca15445124d26cdb4cdbf3c7dd58b
Oladejo Toyyib <oladejotoyyibolalekan@gmail.com>
2023-10-13 13:42:54 +0100

    making limit and exeed limit

--- lib/api-limit.ts
+++ lib/api-limit.ts
@@ -9,5 +9,39 @@ export const increaseApiLimit = async () => {
         return;
     }
 
-    const userApiLimit = await prismadb.userApiLimit
+   const userApiLimit = await prismadb.userApiLimit.findUnique({
+    where: { 
+        userId 
+    }
+   });
+
+   if(userApiLimit) {
+    await prismadb.userApiLimit.update({
+        where: { userId: userId},
+        data: { count: userApiLimit.count + 1}
+    })
+   } else {
+    await prismadb.userApiLimit.create({
+        data: { userId: userId, count: 1 }
+    })
+   }
 }
+
+export const checkApiLimit = async () => {
+    const { userId } = auth();
+    if ( !userId ) {
+        return false;
+    }
+
+    const userApiLimit = await prismadb.userApiLimit.findUnique({
+        where: {
+            userId: userId
+        }
+    })
+
+    if (!userApiLimit || userApiLimit.count < MAX_FREE_COUNTS) {
+        return true;
+    } else {
+        return false;
+    }
+};
